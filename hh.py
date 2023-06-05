d87c0357a54a0788e3e886a5ac244473100b05c6
grits <gritsaj.vlad@gmail.com>
2023-06-05 22:33:00 +0300

    2 com logik

---
+++ hh.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+# Set Chrome options to disable page load strategy
+chrome_options = Options()
+chrome_options.add_argument("--pageLoadStrategy=none")
+
+# Set desired capabilities
+capabilities = DesiredCapabilities().CHROME.copy()
+capabilities['pageLoadStrategy'] = 'eager'
+
+# Instantiate Chrome driver with the configured options and capabilities
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options, desired_capabilities=capabilities)
+
+# Open the URL
+driver.get("https://hh.ru")
+
+# Find and click the element without waiting for the full page load
+driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[2]/div/div/div/div/div[5]/a').click()
