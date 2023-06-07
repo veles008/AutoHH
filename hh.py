4d054055adb9f2a32cdcf6afcb6637dd3a3bb139
grits <gritsaj.vlad@gmail.com>
2023-06-07 22:33:36 +0300

    доделать логику

--- hh.py
+++ hh.py
@@ -1,11 +1,13 @@
 import time
-
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
 # Set Chrome options to disable page load strategy
 chrome_options = Options()
@@ -16,11 +18,26 @@ capabilities = DesiredCapabilities().CHROME.copy()
 capabilities['pageLoadStrategy'] = 'eager'
 
 # Instantiate Chrome driver with the configured options and capabilities
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options, desired_capabilities=capabilities)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options,
+                          desired_capabilities=capabilities)
 
 
 def logic_start(a):
     driver.get("https://hh.ru")
+    time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="HH-React-Root"]/div/div[2]/div/div/div/div/div[5]/a').click()
     time.sleep(3)
     driver.find_element(By.NAME, 'login').send_keys(a)
+    driver.find_element(By.XPATH,
+                        '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div[1]/div/div[2]/form/div[5]/button[1]/span').click()
+
+
+def logik_cod(b):
+    time.sleep(2)
+    driver.find_element(By.XPATH,
+                        '//*[@id="HH-React-Root"]/div/div[3]/div[1]/div/div/div/div/div/div/div/div/div/form/div/div[3]/div/fieldset/input').send_keys(
+        b)
+    time.sleep(2)
+    button = driver.find_element(By.CLASS_NAME, u"verification-submit")
+    driver.implicitly_wait(10)
+    ActionChains(driver).move_to_element(button).click(button).perform()
--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def show_number():
     num = batton_number_vvod.get()
     hh.logic_start(num)
 
+
+def vvod():
+    blon = batton_number_kod.get()
+    hh.logik_cod(blon)
+
+
 # номер телефона
 batton_number = Button(text="Номер телефона", background='#90EE90', font='Times 12', command=show_number)
 batton_number.place(relx=0, rely=0.1, height=50, width=130, )
@@ -18,7 +24,7 @@ batton_number_vvod = Entry(background='#90EE90', font='Times 14')
 batton_number_vvod.place(relx=0.5, rely=0.1, height=50, width=135)
 
 # код который приходит на телефон
-batton_kod = Button(text="Код", background='#90EE90', font='Times 12')
+batton_kod = Button(text="Код", background='#90EE90', font='Times 12', command=vvod)
 batton_kod.place(relx=0, rely=0.32, height=50, width=130, )
 
 batton_number_kod = Entry(background='#90EE90', font='Times 14')
